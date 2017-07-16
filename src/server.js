76d8e071e9540e8bcdba054ac6133ae7833c43f6
setoseng <setoseng@gmail.com>
2017-07-16 15:00:07 -0500

    some new stuff

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 var LolApi = require ('leagueapi');
 
-LolApi.init('RGAPI-d9d23789-ffc3-4e06-b04b-2a7eb5a161a9','na');
-
-LolApi.getChampions(true, function(err, champs){
-  //console.log(champs);
-});
+LolApi.init('RGAPI-20c4b613-38d2-4861-b46b-f489cc0f7c02','na');
+var proList = ["Adrian","Akaadian","Altec","Looper","Pobelter","Arrow","scarra","Impact","Goldenglue","Shiphtur","Shrimp","Doublelift"
+,"Olleh","zig","Cris","Sneaky","Ray","Gate","Reignover","Wildturtle","Hakuho","Pirean","Bjergson","Lourlo","Stunt","Chaser","Nientonsoh"
+,"KiwiKid","BoxBox","Jensen","Seraph","Moon","Link","TheOddOne","Svenskeren","Hard","Trick2G","WingsofDeath","DontMashMe","Hi Im Gosu","Biofrost","Valkrin","Meteos","Ninja","NightBlue3","Piglet"
+,"Reginald","Balls","Stixxay","Dardoch","Huni","Froggen","Fabbbyyy","Matt","Xmithie","Imaqtpie","Dyrus","Darshan","Big","Keith","Huhi","Keene","Hai","Cody Sun","Grigne","Xpecial","Saintvicious"
+,"Yoona","Swifte","LOD","Hauntzer","Santorin","Smoothie","Altec","Lemonnation"];
+// LolApi.getChampions(true, function(err, champs){
+//   //console.log(champs);
+// });
 options = {rankedQueues: ['RANKED_SOLO_5x5','RANKED_TEAM_5x5'], beginIndex: 1, endIndex: 10};
 LolApi.Summoner.getByName('Malicious Donut')
   .then(function (summoner){
@@ -16,5 +20,6 @@ LolApi.getMatchHistory("20557835",options)
   });
 LolApi.getMatch("2074924517")
   .then(function(match){
+    if(match.participantIdentities)
     console.log(match);
   });
